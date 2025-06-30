d5e8466b5605eeea8f4f16bb8f503f7b7483dd73
Kihwan-dev <rlrkf2420@gmail.com>
2025-06-30 10:51:24 +0900

    [feat] Init Project

---
+++ lib/Models/character.dart
@@ -0,0 +1,16 @@
+import 'package:rpg_game/Models/monster.dart';
+
+class Character {
+  String name;
+  int hp;
+  int ap;
+  int dp;
+
+  Character(this.name, this.hp, this.ap, this.dp);
+
+  void attackMonster(Monster monster) {}
+
+  void defend() {}
+
+  void showStatus() {}
+}
